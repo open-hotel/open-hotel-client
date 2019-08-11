d4635383077891e3b6f389719ce6d32df2e426d2
trickstival <trick_stival@hotmail.com>
2019-08-11 00:37:34 -0300

    Added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+/**
+ * Got config from https://dev.to/robertcoopercode/using-eslint-and-prettier-in-a-typescript-project-53jb
+ */
+module.exports =  {
+    parser:  '@typescript-eslint/parser',  // Specifies the ESLint parser
+    extends:  [
+        'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    ],
+    parserOptions:  {
+        ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
+        sourceType:  'module',  // Allows for the use of imports
+    },
+    rules:  {
+        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    },
+}
