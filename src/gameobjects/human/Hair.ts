34905b6e497e8a62d3bab6bb29c9b7b3122ebeba
trickstival <trick_stival@hotmail.com>
2019-08-17 16:51:05 -0300

    feat: init hair loading

--- src/gameobjects/human/Hair.ts
+++ src/gameobjects/human/Hair.ts
@@ -8,7 +8,8 @@ interface HairProps {
 
 export class HumanHair extends HumanLayer {
   constructor(attrs: HairProps) {
-    super('ha', 'human/hair', attrs)
+    super('hr', 'human/hair', attrs, 'hair_F_backbun_h')
     this.sprite.anchor.set(0.5, 0.8)
+    debugger
   }
 }
