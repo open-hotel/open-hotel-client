8050663caf5fb56e14a2bdf3ee4e3555e0fccb6c
Salomão Neto <salomaosnff@yahoo.com>
2019-09-13 00:06:48 -0300

    disable Eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-dupe-class-members': 'off'
+    'no-dupe-class-members': 'off',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
