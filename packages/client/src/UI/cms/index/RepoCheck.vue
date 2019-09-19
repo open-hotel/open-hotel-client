a1cbfea96e3c7664fcf40d98b72dbb661cf9035e
trickstival <trick_stival@hotmail.com>
2019-09-18 21:52:47 -0300

    feat(cms): added images for repository and demo

---
+++ packages/client/src/UI/cms/index/RepoCheck.vue
@@ -0,0 +1,24 @@
+<template>
+  <check-block>
+    <template slot="title">
+      Check out the Repository
+    </template>
+    <template slot="image-link">
+      <img src="/img/github-logo.svg">
+    </template>
+  </check-block>
+</template>
+
+<script>
+import CheckBlock from './CheckBlock'
+
+export default {
+  components: {
+    CheckBlock,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
